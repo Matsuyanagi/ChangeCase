2cd901b8e2b06aff3a519ade7ec1f6d1f8e459a3
Matsuyanagi <1475814+Matsuyanagi@users.noreply.github.com>
2018-07-14 13:05:19 +0900

    Implement SnakeCase, PascalCase, ConstantCase, CamelCase

---
+++ ChangeCase/CaseChanger.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace ChangeCase
+{
+	// rule
+	public class Rule
+	{
+		// word splitter
+		public string Splitter { get; set; } = @"[\-_ ]+|(?<=[a-z])(?=[A-Z])";
+
+		// word joinner
+		public string Joinner { get; set; } = "";
+
+		// word converter
+		public Func< string, string > WordChanger { get; set; } = s => s;
+		public Func< string, int, string > WordChangerWithIndex { get; set; } = ( s, i ) => s;
+	}
+
+	public class CaseChanger
+	{
+		protected static string Change( string inputStr, Rule rule )
+		{
+			var words = Regex.Split( inputStr, rule.Splitter ).Where( s => s.Length > 0 )
+				.Select( s => rule.WordChanger( s ) );
+
+			return String.Join( rule.Joinner, words );
+		}
+
+		protected static string ChangeWithIndex( string inputStr, Rule rule )
+		{
+			var index = 0;
+			var words = Regex.Split( inputStr, rule.Splitter ).Where( s => s.Length > 0 )
+				.Select( s => rule.WordChangerWithIndex( s, index++ ) );
+
+			return String.Join( rule.Joinner, words );
+		}
+
+		// Make the first letter capital letter and the rest lower case. : s.Length > 0 
+		private static string WordTitlize( string s )
+		{
+			return s.Substring( 0, 1 ).ToUpperInvariant() + s.Substring( 1 ).ToLowerInvariant();
+		}
+
+		// snake_case
+		public static string SnakeCase( string inputStr )
+		{
+			var rule = new Rule
+			{
+				Joinner = "_",
+				WordChanger = s => s.ToLower()
+			};
+			return Change( inputStr, rule );
+		}
+
+		// PascalCase
+		public static string PascalCase( string inputStr )
+		{
+			var rule = new Rule
+			{
+				Joinner = "",
+				WordChanger = WordTitlize
+			};
+			return Change( inputStr, rule );
+		}
+
+		// CONSTANT_CASE
+		public static string ConstantCase( string inputStr )
+		{
+			var rule = new Rule
+			{
+				Joinner = "_",
+				WordChanger = s => s.ToUpper()
+			};
+			return Change( inputStr, rule );
+		}
+
+		// camelCase
+		public static string CamelCase( string inputStr )
+		{
+			var rule = new Rule
+			{
+				Joinner = "",
+				WordChangerWithIndex = ( s, i ) => i == 0 ? s.ToLowerInvariant() : WordTitlize( s )
+			};
+			return ChangeWithIndex( inputStr, rule );
+		}
+	}
+}
--- ChangeCase/Program.cs
+++ ChangeCase/Program.cs
@@ -6,10 +6,57 @@ using System.Threading.Tasks;
 
 namespace ChangeCase
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
+	class Program
+	{
+		static void Main( string[] args )
+		{
+			var words = new string[]
+			{
+				"",
+				" ",
+				"-",
+				"_",
+				"__",
+				"----",
+				"a",
+				"aa",
+				"aa-",
+				"-aa",
+				"-aa-",
+				"--aa-",
+				"--aa--",
+				"0",
+				"01",
+				"01-",
+				"a01-",
+				"a01-2",
+				"a01-2b",
+				"a01aad",
+				"0axzAbc",
+				"0ax0Abc",
+				"a---b",
+				"a--b--c--d",
+				"ab--bc--cd--de",
+				"ab--bc -cd--de",
+				"ab--bc  cd_-de",
+				"ab--bc--cd--",
+				"ab--bc--cd-",
+				"ab--bc--c",
+				"ab  bc  c",
+				"testStringWordsAAA",
+				"test-String-Words_AAA",
+				"test-String-Words_012",
+				"TEST String Words_AAA",
+				"TEst string words aaa",
+				"Test_string_words-aaa",
+			};
+
+			foreach ( var word in words ) {
+				Console.WriteLine( "snake_case     : {0,30} : {1,30}", word, CaseChanger.SnakeCase( word ) );
+				Console.WriteLine( "PascalCase     : {0,30} : {1,30}", word, CaseChanger.PascalCase( word ) );
+				Console.WriteLine( "camelCase      : {0,30} : {1,30}", word, CaseChanger.CamelCase( word ) );
+				Console.WriteLine( "CONSTANT_CASE  : {0,30} : {1,30}", word, CaseChanger.ConstantCase( word ) );
+			}
+		}
+	}
 }
